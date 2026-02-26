ba3379156828232d30b064be14033cc270d2dc7a
Ghabryella <mirandaghabryella@gmail.com>
2026-02-26 19:05:55 -0300

    User update

--- src/main/java/com/ghabryella/course/services/UserService.java
+++ src/main/java/com/ghabryella/course/services/UserService.java
@@ -5,6 +5,7 @@ import com.ghabryella.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,11 +25,23 @@ public class UserService {
 
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return repository.save(obj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
